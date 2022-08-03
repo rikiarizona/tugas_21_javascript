e6e871f2da18ba20663bb3a0242b6a9e41fda899
rikiarizona <rikiarizona@gmail.com>
2022-08-03 10:23:24 +0700

    TUGAS 21 JAVASCRIPT
    saya kirimkan tugas niomic

---
+++ index.js
@@ -0,0 +1,7 @@
+function rubahKata() {
+    var kalimat = "Saya belajar masak nasi goreng";
+    console.log(kalimat);
+    var rubah = kalimat.replace("masak", "makan");
+    return rubah;
+}
+console.log(rubahKata());
